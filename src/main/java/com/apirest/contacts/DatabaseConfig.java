ecc77cce968ffaae28a10b338c548483b58e6068
antonialucianapires <antonialucianapires@gmail.com>
2020-04-21 15:15:05 -0300

    configuração do database e deploy no heroku

---
+++ src/main/java/com/apirest/contacts/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package com.apirest.contacts;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
